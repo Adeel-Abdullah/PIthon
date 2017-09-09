8c6e108328e36144eefdf227deccacc369478351
IC-HO-2032 <syedabdullah94@hotmail.com>
2017-09-09 21:27:36 +0500

    added proper imports

--- PIserver.py
+++ PIserver.py
@@ -1,5 +1,5 @@
 import PIthon  
   
-PIConnector.connect_to_Server("Server")  
-value, timestamp = PIConnector.get_tag_snapshot('sinusoid')  
-print('Timestamp: {0} Value: {1}'.format(timestamp, value))  
+PIthon.connect_to_Server("Server")  
+value, timestamp = PIthon.get_tag_snapshot('sinusoid')  
+print('Timestamp: {0} Value: {1}'.format(timestamp, value))
--- Pithon.py
+++ Pithon.py
@@ -1,3 +1,14 @@
+import sys  
+sys.path.append('C:\\Program Files (x86)\\PIPC\\AF\\PublicAssemblies\\4.0\\')  
+import clr  
+clr.AddReference('OSIsoft.AFSDK')
+
+from OSIsoft.AF.PI import *  
+from OSIsoft.AF.Search import *  
+from OSIsoft.AF.Asset import *  
+from OSIsoft.AF.Data import *  
+from OSIsoft.AF.Time import *
+
 def connect_to_Server(serverName):  
     piServers = PIServers()  
     global piServer  
