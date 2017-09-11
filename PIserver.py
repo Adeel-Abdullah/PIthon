24faee3a901ef14a84ab1966ed3028a6de68d5c9
IC-HO-2032 <syedabdullah94@hotmail.com>
2017-09-11 16:21:11 +0500

    dependancies imported

--- PIserver.py
+++ PIserver.py
@@ -1,4 +1,15 @@
-import PIthon  
+import PIthon
+
+import sys
+import clr   
+sys.path.append('C:\\Program Files (x86)\\PIPC\\AF\\PublicAssemblies\\4.0\\')  
+clr.AddReference('OSIsoft.AFSDK')
+
+from OSIsoft.AF.PI import *  
+from OSIsoft.AF.Search import *  
+from OSIsoft.AF.Asset import *  
+from OSIsoft.AF.Data import *  
+from OSIsoft.AF.Time import *
   
 PIthon.connect_to_Server("Server")  
 value, timestamp = PIthon.get_tag_snapshot('sinusoid')  
--- Pithon.py
+++ Pithon.py
@@ -2,16 +2,6 @@
 functions to connect to the PI server and fetch the current tag value
 
 """
-import sys
-import clr   
-sys.path.append('C:\\Program Files (x86)\\PIPC\\AF\\PublicAssemblies\\4.0\\')  
-clr.AddReference('OSIsoft.AFSDK')
-
-from OSIsoft.AF.PI import *  
-from OSIsoft.AF.Search import *  
-from OSIsoft.AF.Asset import *  
-from OSIsoft.AF.Data import *  
-from OSIsoft.AF.Time import *
 
 def connect_to_Server(serverName):  
     piServers = PIServers()  
